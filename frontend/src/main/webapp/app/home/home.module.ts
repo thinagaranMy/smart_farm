cfa9a3bcf177cd586687f276a4b562ad94425faf
K Haridass, Thinagaran <Thinagaran.KHaridass@sicpa.com>
2019-11-04 22:42:49 +0800

    mock dashboard admin

--- frontend/src/main/webapp/app/home/home.module.ts
+++ frontend/src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,14 @@ import { RouterModule } from '@angular/router';
 import { SmarfarmSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { SmarfarmDashboardModule } from '../dashboard/dashboard.module';
 
 @NgModule({
-  imports: [SmarfarmSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [
+    SmarfarmSharedModule, 
+    SmarfarmDashboardModule, 
+    RouterModule.forChild([HOME_ROUTE])
+    ],
   declarations: [HomeComponent]
 })
 export class SmarfarmHomeModule {}
